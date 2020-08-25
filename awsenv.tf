4f85768d84311f3a463588e7af76464126526afe
arjunachari12 <68580129+arjunachari12@users.noreply.github.com>
2020-08-25 18:45:56 +0530

    Update awsenv.tf

---
+++ awsenv.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAILQ66A22BFQQGF7Q"
+  secret_key = "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"
+}
+
+variable "subnet_prefix" {
+  description = "cidr block for the subnet"
+}
+
+resource "aws_vpc" "prod-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "production"
+  }
+}
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id            = aws_vpc.prod-vpc.id
+  cidr_block        = var.subnet_prefix[0].cidr_block
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = var.subnet_prefix[0].name
+  }
+
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket-arjunnn"
+  acl    = "private"
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_subnet" "subnet-2" {
+  vpc_id            = aws_vpc.prod-vpc.id
+  cidr_block        = var.subnet_prefix[1].cidr_block
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = var.subnet_prefix[1].name
+  }
+}
